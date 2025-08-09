081c7457ee771b5d6b70953c74b1fc389929e9af
robby <robbyabraar5@gmail,com>
2025-08-09 18:21:59 +0700

    ubah isi index bcrypt

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 app.use(express.json());
